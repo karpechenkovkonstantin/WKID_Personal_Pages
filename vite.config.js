b56ac26b42f9f4abb64afe890235edfc0917a251
Karpechenkov Konstantin <142743125+karpechenkovkonstantin@users.noreply.github.com>
2025-04-30 00:09:02 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import devConfig from './vite.config.dev'
 import prodConfig from './vite.config.prod'
 
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve' && mode === 'development') {
     return devConfig
   } else {
     return prodConfig
