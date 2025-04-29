61f2c8f4f9466a157e1c8f790a67982dd61134c8
Karpechenkov Konstantin <142743125+karpechenkovkonstantin@users.noreply.github.com>
2025-04-29 17:02:10 +0300

    Асинхронная подгрузка ТГ скрипта

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
+// Load Telegram Web App script
+const script = document.createElement('script');
+script.src = 'https://telegram.org/js/telegram-web-app.js?56';
+script.async = true;
+document.head.appendChild(script);
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
